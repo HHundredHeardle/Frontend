8eec6d6825042191f0202899c90a4b11e58b5ad7
Joshua Linehan <jlinehan@student.unimelb.edu.au>
2024-12-06 15:06:05 +1100

    Added tests

---
+++ test/widget/header_test.dart
@@ -0,0 +1,27 @@
+/// Hottest Hundred Heardle
+/// header_test.dart
+///
+/// Widget tests for header
+///
+/// Authors: Joshua Linehan
+library;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hottest_hundred_heardle/header.dart';
+
+void main() {
+  testWidgets("Title is correct", (WidgetTester tester) async {
+    // Build widget and trigger frame
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: HHHeader(),
+        ),
+      ),
+    );
+
+    // verify that title exists
+    expect(find.text("Hottest Hundred Heardle"), findsOneWidget);
+  });
+}
---
+++ test/widget/main_test.dart
@@ -0,0 +1,20 @@
+/// Hottest Hundred Heardle
+/// main_test.dart
+///
+/// Widget tests for main screen
+///
+/// Authors: Joshua Linehan
+library;
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hottest_hundred_heardle/main.dart';
+
+void main() {
+  testWidgets("Title is displayed", (WidgetTester tester) async {
+    // Build app and trigger frame
+    await tester.pumpWidget(const HHundredHeardle());
+
+    // verify that title exists
+    expect(find.text("Hottest Hundred Heardle"), findsOneWidget);
+  });
+}
