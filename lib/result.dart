ead1234acf6b8d42e1954c743992b9a8ac22a13b
Joshua Linehan <jlinehan@student.unimelb.edu.au>
2025-05-26 17:40:04 +1000

    implemented result

--- lib/result.dart
+++ lib/result.dart
@@ -8,24 +8,42 @@ library;
 
 import 'package:flutter/material.dart';
 
+import 'game_controller.dart';
+
 /// Displays the result of the game
 class HHResults extends StatelessWidget {
   const HHResults({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [_HHResult(), _HHAnswer(), _HHStreak()],
+    return Column(
+      children: [_HHResult(), const _HHAnswer(), const _HHStreak()],
     );
   }
 }
 
 /// Displays number of guesses or failure message
 class _HHResult extends StatelessWidget {
-  const _HHResult();
+  final Future<Result> _result = GameController().result;
+
+  _HHResult();
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return FutureBuilder(
+      future: _result,
+      builder: (context, snapshot) {
+        int guesses = GameController().numGuesses();
+        return (snapshot.connectionState == ConnectionState.done)
+            ? Text(
+                switch (snapshot.data!) {
+                  Result.win =>
+                    "You got today's answer in $guesses guess${(guesses != 1) ? "es" : ""}",
+                  Result.lose => "You didn't get today's answer",
+                },
+              )
+            : Container();
+      },
+    );
   }
 }
 
